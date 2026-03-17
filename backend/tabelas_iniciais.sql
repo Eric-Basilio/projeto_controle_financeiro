9f0378d48fcb92c3ec62e6c566681236aed9df09
Eric Basilio <eric__basilio@outlook.com>
2026-03-17 16:52:31 -0300

    correção de erros na criação das tabelas no banco de dados

--- backend/tabelas_iniciais.sql
+++ backend/tabelas_iniciais.sql
@@ -12,6 +12,12 @@ create table if not exists usuario (
   FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
   );
 
+  CREATE TABLE IF NOT EXISTS categoria_compra (
+    id_categoria_compra SERIAL PRIMARY KEY,
+    id_compra INT,
+    categoria VARCHAR(50)
+  );
+
 create table if not exists compra (
   id_compra SERIAL PRIMARY KEY,
   id_usuario INT,
@@ -23,22 +29,14 @@ create table if not exists compra (
   data_compra DATE,
   status_compra BOOLEAN, /* pago  ou não pago */
   id_categoria INT,
-  FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
+  FOREIGN KEY (id_categoria) REFERENCES categoria_compra(id_categoria_compra),
   FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
   FOREIGN KEY (id_banco) REFERENCES banco(id_banco)
-  );
-
-  CREATE TABLE IF NOT EXISTS categoria_compra (
-    id_categoria_compra SERIAL PRIMARY KEY,
-    id_compra INT,
-    categoria VARCHAR(50),
   );
 
   create table if not exists dono_compra (
     id_dono SERIAL PRIMARY KEY,
     id_compra INT,
     nome_dono VARCHAR(100),
-    FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
-    FOREIGN KEY (id_banco) REFERENCES banco(id_banco)
+    FOREIGN KEY (id_compra) REFERENCES compra(id_compra)
     );
-
