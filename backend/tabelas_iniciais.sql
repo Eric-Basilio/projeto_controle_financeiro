8359398e4489a8760aa11f5978eed2aa7b472f94
Eric Basilio <eric__basilio@outlook.com>
2026-03-17 00:15:32 -0300

    Correção do banco de dados

--- backend/tabelas_iniciais.sql
+++ backend/tabelas_iniciais.sql
@@ -1,50 +1,44 @@
-creat table if not exists usuario {
-  id_usuario int primary key,
-  nome_usuario varchar (100),
-  senha_hassh varchar (100),
-  saldo float,
-} 
+create table if not exists usuario (
+  id_usuario SERIAL PRIMARY KEY,
+  nome_usuario VARCHAR(100) UNIQUE,
+  senha_hash VARCHAR(100),
+  saldo NUMERIC(10,2));
 
-creat table if not exists compra {
-  id_compra int primary key,
-  id_usuario int,
-  id_banco int,
-  categoria_compra varchar (100),
-  dono_compra varchar (100),
-  valor_compra float,
-  compra_parcelada boolean,
-  numero_parcelas int,
-  data_compra date,
-  status_compra boolean, /* pago  ou não pago */
-  }
+  create table if not exists banco (
+  id_banco SERIAL PRIMARY KEY,
+  id_usuario INT,
+  nome_banco VARCHAR(100),
+  total_divida NUMERIC(10,2),
+  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+  );
 
-  creat table if not exists dono_compra {
-    id_dono int primary key,
-    id_compra int,
-    id_banco int,
-    total_divida_dono float,
-    }
+create table if not exists compra (
+  id_compra SERIAL PRIMARY KEY,
+  id_usuario INT,
+  id_banco INT,
+  dono_compra VARCHAR(100),
+  valor_compra NUMERIC(10,2),
+  compra_parcelada BOOLEAN,
+  numero_parcelas INT,
+  data_compra DATE,
+  status_compra BOOLEAN, /* pago  ou não pago */
+  id_categoria INT,
+  FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
+  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+  FOREIGN KEY (id_banco) REFERENCES banco(id_banco)
+  );
 
+  CREATE TABLE IF NOT EXISTS categoria_compra (
+    id_categoria_compra SERIAL PRIMARY KEY,
+    id_compra INT,
+    categoria VARCHAR(50),
+  )
 
+  create table if not exists dono_compra (
+    id_dono SERIAL PRIMARY KEY,
+    id_compra INT,
+    nome_dono VARCHAR(100),
+    FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
+    FOREIGN KEY (id_banco) REFERENCES banco(id_banco)
+    );
 
-  creat table if not exists banco {
-  id_banco int [primary key]
-  id_compra int
-  id_usuario int
-  nome_banco varchar (100)
-  total_divida float
-  }
-
-Ref: "usuario"."id_usuario" < "compra"."id_usuario"
-
-Ref: "banco"."id_banco" < "compra"."id_banco"
-
-Ref: "banco"."id_usuario" < "usuario"."id_usuario"
-
-
-
-Ref: "banco"."id_compra" < "compra"."id_compra"
-
-Ref: "dono_compra"."id_compra" < "compra"."id_compra"
-
-Ref: "dono_compra"."id_banco" < "banco"."id_banco"
